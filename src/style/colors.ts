3dcf57a85412d59d8f7d7cbadfe9d85806032ca2
Pavel Yakovlev <xcvb2@yandex.ru>
2021-11-21 23:17:12 +0500

    Declare the `ThemeColors` type

--- src/style/colors.ts
+++ src/style/colors.ts
@@ -1,6 +1,21 @@
-export const lightTheme = {
+export type ThemeColors = {
+  [color in
+    | "main"
+    | "header"
+    | "headerAlt"
+    | "background"
+    | "backgroundSecondary"
+    | "backgroundTertiary"
+    | "text"
+    | "textSecondary"
+    | "textSecondaryActive"
+    | "textAlt"]: string;
+};
+
+export const lightTheme: ThemeColors = {
   main: "#db4c3f",
   header: "#db4c3f",
+  headerAlt: "rgba(255,255,255,0.2)",
   background: "#ffffff",
   backgroundSecondary: "#fafafa",
   backgroundTertiary: "#eeeeee",
@@ -10,27 +25,28 @@ export const lightTheme = {
   textAlt: "#ffffff",
 };
 
-export const noirTheme = {
+export const noirTheme: ThemeColors = {
   ...lightTheme,
   main: "#3d3d3d",
   header: "#3d3d3d",
 };
 
-export const neutralTheme = {
+export const neutralTheme: ThemeColors = {
   ...lightTheme,
   main: "#db4c3f",
   header: "#f7f7f7",
 };
 
-export const orangeTheme = {
+export const orangeTheme: ThemeColors = {
   ...lightTheme,
   main: "#ff9000",
   header: "#ff9000",
 };
 
-export const darkTheme = {
+export const darkTheme: ThemeColors = {
   main: "#db4c3f",
   header: "#282828",
+  headerAlt: "rgba(255,255,255,0.2)",
   background: "#1e1e1e",
   backgroundSecondary: "#282828",
   backgroundTertiary: "#363636",
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type { ThemeColors } from "style/colors";
