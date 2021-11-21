dd53d07ca103ed23f4e4595efd8b4ce5996ea9a6
Pavel Yakovlev <xcvb2@yandex.ru>
2021-11-21 19:04:25 +0500

    Add the `createContext` helper
    A helper for a creation of a Context and a Provider with no upfront
    default value and without having to check for undefined all the time.

---
+++ src/context/createContext.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * A helper for a creation of a Context and a Provider with no upfront
+ * default value and without having to check for undefined all the time.
+ */
+const createContext = <A extends Record<string, unknown> | null>() => {
+  const context = React.createContext<A | undefined>(undefined);
+  const useContext = () => {
+    const c = React.useContext(context);
+    if (c === undefined) {
+      throw new Error("useContext must be inside a Provider with a value");
+    }
+    return c;
+  };
+  return [useContext, context.Provider, context] as const;
+};
+
+export default createContext;
