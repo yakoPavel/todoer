51687d4689a9db2a5e3ad15a22c8f036cecf9d36
Pavel Yakovlev <xcvb2@yandex.ru>
2021-11-30 20:52:41 +0500

    Add more media queries

---
+++ src/style/mediaQueries.ts
@@ -0,0 +1,4 @@
+export const preSm = "@media (min-width: 18.75em)";
+export const sm = "@media (min-width: 25em)";
+export const preMd = "@media (min-width: 38.5em)";
+export const md = "@media (min-width: 48em)";
--- src/style/mediaQueries.tsx
+++
@@ -1,2 +0,0 @@
-export const preMd = "@media (min-width: 38.5em)";
-export const md = "@media (min-width: 48em)";
