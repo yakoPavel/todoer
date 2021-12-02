95760703d911f7f4793c76156410b1ee463c1ce0
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-02 14:01:38 +0500

    Add the `Button` component

---
+++ src/components/UI/Button/Button.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import Button from "./Button";
+
+export default {
+  title: "UI/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Button text</Button>
+);
+
+export const Primary = Template.bind({});
+
+export const PrimaryDisabled = Template.bind({});
+PrimaryDisabled.args = {
+  disabled: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: "secondary",
+};
+
+export const SecondaryDisabled = Template.bind({});
+SecondaryDisabled.args = {
+  variant: "secondary",
+  disabled: true,
+};
---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled/macro";
+import React from "react";
+import tinyColor from "tinycolor2";
+
+/**
+ * Slightly adjusts the color according to its lightness or darkness to the
+ * opposite direction.
+ */
+function getShiftedColor(initialColor: string) {
+  const color = tinyColor(initialColor);
+
+  if (color.isLight()) return color.darken().toHexString();
+  return color.lighten().toHexString();
+}
+
+const PrimaryButton = styled.button`
+  background-color: ${({ theme }) => theme.main};
+  color: ${({ theme }) => theme.textAlt};
+  font-size: 1.3rem;
+  padding: 0.6em 1em;
+  border: 1px solid ${({ theme }) => theme.main};
+  border-radius: 5px;
+  transition: background-color 0.2s linear;
+
+  &:not(:disabled):hover {
+    background-color: ${({ theme }) => getShiftedColor(theme.main)};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-color: ${({ theme }) =>
+      tinyColor(theme.main).desaturate(30).toHexString()};
+    color: ${({ theme }) =>
+      tinyColor(theme.textAlt).desaturate(30).toHexString()};
+  }
+`;
+
+const SecondaryButton = styled(PrimaryButton)`
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.separators};
+
+  &:not(:disabled):hover {
+    background-color: ${({ theme }) => getShiftedColor(theme.background)};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background-color: ${({ theme }) =>
+      tinyColor(theme.background).desaturate(30).toHexString()};
+    color: ${({ theme }) => tinyColor(theme.text).desaturate(30).toHexString()};
+  }
+`;
+
+type ButtonProps = React.ComponentProps<"button"> & {
+  /** A variant of the button. */
+  variant?: "primary" | "secondary";
+};
+
+/**
+ * It is a styled button.
+ */
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  ...otherProps
+}) => {
+  if (variant === "primary") return <PrimaryButton {...otherProps} />;
+  return <SecondaryButton {...otherProps} />;
+};
+
+export default Button;
