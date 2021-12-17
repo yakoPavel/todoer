dcb299af6919551aa9a6bf08f01410732b1d466b
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-17 18:43:34 +0500

    Add the `UserContext`
    This context keeps track of the authentication status and
    exposes the user's information if he is logged in.

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,18 @@
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import React from "react";
+
+import createContext from "./createContext";
+
+const [useUserContext, ContextProvider] = createContext<User | null>();
+
+export const UserContextProvider: React.FC = ({ children }) => {
+  const [user, setUser] = React.useState<User | null>(null);
+
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, setUser);
+
+  return <ContextProvider value={user}>{children}</ContextProvider>;
+};
+
+export default useUserContext;
