f8fe868c2c0aa7312db6ca4abfc2cdf6e6b634d6
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-17 18:18:08 +0500

    Add the `LoadingContext`
    This context is intended to contain a loading state flag
    and a function to set it.

---
+++ src/context/LoadingContext.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import createContext from "./createContext";
+
+type LoadingState = {
+  /** Whether or not the screen in the loading state. */
+  isScreenLoading: boolean;
+  /** Sets the loading state of the screen. */
+  setIsScreenLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const [useLoadingState, ContextProvider] = createContext<LoadingState>();
+
+export const LoadingStateContextProvider: React.FC = ({ children }) => {
+  const [isScreenLoading, setIsScreenLoading] = React.useState(false);
+
+  return (
+    <ContextProvider value={{ isScreenLoading, setIsScreenLoading }}>
+      {children}
+    </ContextProvider>
+  );
+};
+
+export default useLoadingState;
