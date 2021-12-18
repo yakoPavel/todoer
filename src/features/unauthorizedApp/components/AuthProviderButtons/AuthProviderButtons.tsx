25646daca740ced20347653983dbf02a1b58d9be
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-18 18:37:30 +0500

    Connect the buttons to the auth logic

--- src/features/unauthorizedApp/components/AuthProviderButtons/AuthProviderButtons.tsx
+++ src/features/unauthorizedApp/components/AuthProviderButtons/AuthProviderButtons.tsx
@@ -1,16 +1,42 @@
 import useLoadingState from "context/LoadingContext";
+import { useAsyncTask } from "hooks/useAsyncTask";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+import { signInThroughProvider } from "../../utils/signInThroughProvider";
 import AuthProviderButton from "../AuthProviderButton/AuthProviderButton";
 
 const AuthProviderButtons = () => {
+  const navigate = useNavigate();
   const { isScreenLoading, setIsScreenLoading } = useLoadingState();
+  const { isSuccess, isLoading, run } = useAsyncTask(signInThroughProvider);
+
+  if (isSuccess) navigate("/", { replace: true });
+
+  React.useEffect(() => {
+    setIsScreenLoading(isLoading);
+  });
 
   return (
     <>
-      <AuthProviderButton variant="google" isDisabled={isScreenLoading} />
-      <AuthProviderButton variant="facebook" isDisabled={isScreenLoading} />
-      <AuthProviderButton variant="apple" isDisabled={isScreenLoading} />
+      <AuthProviderButton
+        onClick={() => run("google")}
+        variant="google"
+        isDisabled={isScreenLoading}
+        isLoading={isLoading}
+      />
+      <AuthProviderButton
+        onClick={() => run("facebook")}
+        variant="facebook"
+        isDisabled={isScreenLoading}
+        isLoading={isLoading}
+      />
+      <AuthProviderButton
+        onClick={() => run("yahoo")}
+        variant="yahoo"
+        isDisabled={isScreenLoading}
+        isLoading={isLoading}
+      />
     </>
   );
 };
