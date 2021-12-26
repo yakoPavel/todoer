c9072a0c51c01c919f4295bebdd64ce652da4335
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-26 19:44:07 +0500

    Wrap section buttons into an h3

--- src/features/authorizedApp/features/SideMenu/components/MenuSection/MenuSection.tsx
+++ src/features/authorizedApp/features/SideMenu/components/MenuSection/MenuSection.tsx
@@ -30,37 +30,37 @@ const MenuSection: React.FC<MenuSectionProps> = ({
   return (
     <Accordion allowToggle width="100%">
       <AccordionItem border="none">
-        {/* <h3 width="100%"> */}
-        <AccordionButton
-          _hover={{ background: "unset" }}
-          _focus={{
-            boxShadow: "unset",
-          }}
-          _focusVisible={{
-            border: `2px solid ${theme.focus}`,
-          }}
-          fontSize="md"
-          display="inline-block"
-        >
-          <Flex
-            alignItems="center"
-            color={theme.text}
-            width="100%"
-            justifyContent="space-between"
+        <h3>
+          <AccordionButton
+            _hover={{ background: "unset" }}
+            _focus={{
+              boxShadow: "unset",
+            }}
+            _focusVisible={{
+              border: `2px solid ${theme.focus}`,
+            }}
+            fontSize="md"
+            display="inline-block"
           >
-            <AccordionIcon alignSelf="flexStart" />
-            <Text
-              marginLeft="1rem"
-              fontSize="md"
-              fontWeight="700"
-              lineHeight="1"
+            <Flex
+              alignItems="center"
+              color={theme.text}
+              width="100%"
+              justifyContent="space-between"
             >
-              {sectionTitle}
-            </Text>
-            <Box marginLeft="1rem">{rightSlot}</Box>
-          </Flex>
-        </AccordionButton>
-        {/* </h3> */}
+              <AccordionIcon alignSelf="flexStart" />
+              <Text
+                marginLeft="1rem"
+                fontSize="md"
+                fontWeight="700"
+                lineHeight="1"
+              >
+                {sectionTitle}
+              </Text>
+              <Box marginLeft="1rem">{rightSlot}</Box>
+            </Flex>
+          </AccordionButton>
+        </h3>
         <AccordionPanel>{sectionContent}</AccordionPanel>
       </AccordionItem>
     </Accordion>
