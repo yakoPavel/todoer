e89a3736043f57c3d89d687de248a9bbe87bd43f
Pavel Yakovlev <xcvb2@yandex.ru>
2021-12-27 19:17:24 +0500

    Add the `EventWithProcessedField`
    This type represents a react event with the `processed` field.
    This field signals that the event has been already handled.

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,10 @@
+import { SyntheticEvent } from "react";
+
 export type { ThemeColors } from "style/colors";
+
+/**
+ * A react event with the `processed` field. This field signals that the
+ * event has been already handled.
+ */
+export type EventWithProcessedField<EventType extends SyntheticEvent> =
+  EventType & { processed?: boolean };
