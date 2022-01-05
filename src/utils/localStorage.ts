edbc6ef4f79efd5a7a307e767baefbd40bbc8bb9
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-05 20:17:14 +0500

    Fix the return type of the `getFromLocalStorage`

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -3,10 +3,24 @@ function saveToLocalStorage(key: string, value: unknown) {
   window.localStorage.setItem(key, JSON.stringify(value));
 }
 
+type GetFromLocalStorageReturn<Data> = Data extends infer ActualData
+  ? unknown extends ActualData
+    ? unknown | null
+    : ActualData
+  : never;
+
 /** Retrieves a value from the local storage. */
-function getFromLocalStorage<T>(key: string, defaultValue?: T) {
+function getFromLocalStorage<Data = unknown>(
+  key: string,
+  defaultValue?: Data,
+): GetFromLocalStorageReturn<Data> {
   const itemInTheStore = window.localStorage.getItem(key);
-  return itemInTheStore ? (JSON.parse(itemInTheStore) as T) : defaultValue;
+
+  if (itemInTheStore === null) {
+    return (defaultValue ?? itemInTheStore) as GetFromLocalStorageReturn<Data>;
+  }
+
+  return JSON.parse(itemInTheStore);
 }
 
 export { getFromLocalStorage, saveToLocalStorage };
