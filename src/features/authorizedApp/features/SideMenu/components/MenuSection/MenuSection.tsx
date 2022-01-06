db0f158157adc63e9de98fdb87d52ae5c85a1510
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-06 19:55:26 +0500

    Persist the collapsed state of the `MenuSection`

--- src/features/authorizedApp/features/SideMenu/components/MenuSection/MenuSection.tsx
+++ src/features/authorizedApp/features/SideMenu/components/MenuSection/MenuSection.tsx
@@ -9,8 +9,10 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useTheme } from "@emotion/react";
+import { SIDE_MENU } from "config/localStorage";
 import React from "react";
 import { EventWithProcessedField } from "types";
+import * as localStorage from "utils/localStorage";
 
 type MenuSectionProps = {
   /** A title of this section. */
@@ -30,11 +32,38 @@ const MenuSection: React.FC<MenuSectionProps> = ({
   className,
 }) => {
   const theme = useTheme();
-  const [expanded, setExpanded] = React.useState(false);
+  const [collapsed, setCollapsed] = React.useState(() =>
+    localStorage
+      .getFromLocalStorage(SIDE_MENU.COLLAPSED_SECTIONS, [] as string[])
+      .includes(sectionTitle),
+  );
 
   const onToggle = (event: EventWithProcessedField<React.MouseEvent>) => {
     if (event.processed) return;
-    setExpanded((prevState) => !prevState);
+    setCollapsed((prevState) => {
+      const newState = !prevState;
+
+      const collapsedSections = localStorage.getFromLocalStorage(
+        SIDE_MENU.COLLAPSED_SECTIONS,
+        [] as string[],
+      );
+      const thisSectionIndex = collapsedSections.findIndex(
+        (collapsedSection) => collapsedSection === sectionTitle,
+      );
+
+      if (thisSectionIndex === -1) {
+        collapsedSections.push(sectionTitle);
+      } else {
+        collapsedSections.splice(thisSectionIndex, 1);
+      }
+
+      localStorage.saveToLocalStorage(
+        SIDE_MENU.COLLAPSED_SECTIONS,
+        collapsedSections,
+      );
+
+      return newState;
+    });
   };
 
   return (
@@ -42,7 +71,7 @@ const MenuSection: React.FC<MenuSectionProps> = ({
       allowToggle
       width="100%"
       className={className}
-      index={expanded ? 0 : -1}
+      index={collapsed ? -1 : 0}
     >
       <AccordionItem border="none">
         <h3>
