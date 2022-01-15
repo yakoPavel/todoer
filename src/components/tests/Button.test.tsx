fc9231f26b6923c5b39e136a7f7790333f76095e
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-15 20:11:12 +0500

    Add test for the `Button` component

---
+++ src/components/tests/Button.test.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jest/no-standalone-expect */
+import Chance from "chance";
+import Button from "components/Button/Button";
+import cases from "jest-in-case";
+import React from "react";
+import { render, screen } from "test/testUtils";
+
+const SEED = 12345;
+const chance = new Chance(SEED);
+
+describe("The `Button` component", () => {
+  cases(
+    "Rendering",
+    (opts) => {
+      const buttonText = chance.word();
+      render(<Button variant={opts.variant}>{buttonText}</Button>);
+
+      expect(
+        screen.getByRole("button", { name: buttonText }),
+      ).toBeInTheDocument();
+    },
+    [
+      {
+        name: "Can render the button with a 'primary' variant",
+        variant: "primary" as const,
+      },
+      {
+        name: "Can render the button with a 'secondary' variant",
+        variant: "secondary" as const,
+      },
+    ],
+  );
+});
