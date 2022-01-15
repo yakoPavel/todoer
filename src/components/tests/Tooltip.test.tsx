74418199a01c54264a2eb29b3e47287f6e97b2a9
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-15 19:41:34 +0500

    Refactor the `Tooltip` tests

--- src/components/tests/Tooltip.test.tsx
+++ src/components/tests/Tooltip.test.tsx
@@ -8,33 +8,43 @@ const chance = new Chance();
 
 function renderTooltip(shortcut?: string[]) {
   const tooltipText = chance.word();
+  const triggerText = chance.sentence();
 
   const renderResult = render(
     <Tooltip tooltipText={tooltipText} shortcut={shortcut} isOpen>
-      <div>Trigger</div>
+      <div>{triggerText}</div>
     </Tooltip>,
   );
 
-  return { tooltipText, ...renderResult };
+  return { tooltipText, triggerText, ...renderResult };
 }
 
-test("renders a tooltip with a plain text", () => {
-  const { tooltipText } = renderTooltip();
+describe("The `Tooltip` component", () => {
+  describe("Rendering", () => {
+    test("Renders a tooltip trigger component", () => {
+      const { triggerText } = renderTooltip();
+      expect(screen.getByText(triggerText)).toBeInTheDocument();
+    });
 
-  expect(
-    screen.getByRole("tooltip", { name: tooltipText }),
-  ).toBeInTheDocument();
-});
+    test("Can render a tooltip with a plain text as a label", () => {
+      const { tooltipText } = renderTooltip();
 
-test("renders a tooltip with a keyboard shortcut", () => {
-  const sampleShortcut = ["shift", "M"];
-  const { tooltipText } = renderTooltip(["shift", "M"]);
+      expect(
+        screen.getByRole("tooltip", { name: tooltipText }),
+      ).toBeInTheDocument();
+    });
 
-  const tooltip = screen.getByRole("tooltip", {
-    name: RegExp(tooltipText),
+    test("Can render a tooltip with a keyboard shortcut", () => {
+      const sampleShortcut = ["shift", "M"];
+      const { tooltipText } = renderTooltip(["shift", "M"]);
+
+      const tooltip = screen.getByRole("tooltip", {
+        name: RegExp(tooltipText),
+      });
+
+      sampleShortcut.forEach((key) =>
+        expect(tooltip.textContent).toMatch(RegExp(key)),
+      );
+    });
   });
-
-  sampleShortcut.forEach((key) =>
-    expect(tooltip.textContent?.includes(key)).toBe(true),
-  );
 });
