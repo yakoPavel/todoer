d649f82f904d750e186be7504e8364e3b4bb822d
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-15 21:12:43 +0500

    Add test for the `StyledLink` component

---
+++ src/components/tests/StyledLink.test.tsx
@@ -0,0 +1,25 @@
+import Chance from "chance";
+import StyledLink from "components/StyledLink/StyledLink";
+import React from "react";
+import { render, screen } from "test/testUtils";
+
+const SEED = 12345;
+const chance = new Chance(SEED);
+
+describe("The `StyledLink` component", () => {
+  const href = chance.url();
+  const to = `/${chance.word()}`;
+  const linkText = chance.word();
+
+  test("Can render a link when the `href` prop is passed", () => {
+    render(<StyledLink href={href}>{linkText}</StyledLink>);
+
+    expect(screen.getByRole("link", { name: linkText })).toBeInTheDocument();
+  });
+
+  test("Can render a link when the `to` prop is passed", () => {
+    render(<StyledLink to={to}>{linkText}</StyledLink>);
+
+    expect(screen.getByRole("link", { name: linkText })).toBeInTheDocument();
+  });
+});
