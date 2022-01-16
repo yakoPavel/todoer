3a5c8173796f019207429174f0ab02096906b0f8
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-16 18:08:35 +0500

    Wrap test to the 'describe' block

--- src/features/unauthorizedApp/features/Form/components/tests/AuthPasswordInput.test.tsx
+++ src/features/unauthorizedApp/features/Form/components/tests/AuthPasswordInput.test.tsx
@@ -3,18 +3,20 @@ import { render, screen, userEvent } from "test/testUtils";
 
 import { Default as AuthPasswordInput } from "../AuthPasswordInput/AuthPasswordInput.stories";
 
-test("hides and shows the password when the 'password visibility button' is clicked", () => {
-  render(<AuthPasswordInput />);
-  const inputField = screen.getByLabelText(/^password$/i);
-  const changeVisibilityButton = screen.getByRole("button", {
-    name: /(show|hide) password/i,
+describe("The `AuthPasswordInput` component", () => {
+  test("When the 'password visibility button' is clicked, hides and shows the password", () => {
+    render(<AuthPasswordInput />);
+    const inputField = screen.getByLabelText(/^password$/i);
+    const changeVisibilityButton = screen.getByRole("button", {
+      name: /(show|hide) password/i,
+    });
+
+    expect(inputField).toHaveAttribute("type", "password");
+
+    userEvent.click(changeVisibilityButton);
+    expect(inputField).toHaveAttribute("type", "text");
+
+    userEvent.click(changeVisibilityButton);
+    expect(inputField).toHaveAttribute("type", "password");
   });
-
-  expect(inputField).toHaveAttribute("type", "password");
-
-  userEvent.click(changeVisibilityButton);
-  expect(inputField).toHaveAttribute("type", "text");
-
-  userEvent.click(changeVisibilityButton);
-  expect(inputField).toHaveAttribute("type", "password");
 });
