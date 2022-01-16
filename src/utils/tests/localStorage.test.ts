16085e70cc8720218e3ba3b71caf0518f4f1493d
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-16 14:38:41 +0500

    Add tests for the `localStorage`

--- src/utils/tests/localStorage.test.ts
+++ src/utils/tests/localStorage.test.ts
@@ -1,40 +1,49 @@
+import * as fc from "fast-check";
+
 import * as localStorage from "../localStorage";
 
-const testCases = [
-  { key: "SOME_KEY", value: { name: "Nick", age: 43 } },
-  { key: "SOME_OTHER_KEY", value: 435 },
-  { key: "ANOTHER_ONE", value: "Hi" },
-];
-
-test("correctly saves data to the storage", () => {
-  const mockedSetItem = jest.fn();
-  const originalSetItem = global.Storage.prototype.setItem;
-  global.Storage.prototype.setItem = mockedSetItem;
-
-  testCases.forEach(({ key, value }, index) => {
-    localStorage.saveToLocalStorage(key, value);
-    expect(mockedSetItem).toHaveBeenCalledTimes(index + 1);
-    expect(mockedSetItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+describe("The `localStorage` util", () => {
+  afterEach(() => {
+    global.localStorage.clear();
   });
 
-  global.Storage.prototype.setItem = originalSetItem;
-});
-
-test("correctly returns saved values", () => {
-  testCases.forEach(({ key, value }) => {
-    localStorage.saveToLocalStorage(key, value);
-    expect(localStorage.getFromLocalStorage(key)).toEqual(value);
+  test("Correctly saves and reads data from the storage", () => {
+    fc.assert(
+      fc.property(
+        fc.string(),
+        fc.anything({
+          values: [fc.boolean(), fc.integer(), fc.string()],
+        }),
+        (key, value) => {
+          localStorage.saveToLocalStorage(key, value);
+          expect(localStorage.getFromLocalStorage(key)).toEqual(value);
+        },
+      ),
+    );
   });
 
-  global.localStorage.clear();
-});
+  describe("When the requested value wasn't found and the default value is specified", () => {
+    test("Returns the default value", () => {
+      fc.assert(
+        fc.property(
+          fc.string(),
+          fc.anything({
+            values: [fc.boolean(), fc.integer(), fc.string()],
+          }),
+          (key, defaultValue) => {
+            expect(localStorage.getFromLocalStorage(key, defaultValue)).toBe(
+              defaultValue,
+            );
+          },
+        ),
+      );
+    });
+  });
 
-test("returns a default value if the requested one wasn't found", () => {
-  const defaultValue = "SOME_DEFAULT";
-  expect(
-    localStorage.getFromLocalStorage(
-      "KEY_THAT_IS_NOT_IN_THE_STORAGE",
-      defaultValue,
-    ),
-  ).toBe(defaultValue);
+  describe("When the requested value wasn't found and the default value is not specified", () => {
+    test("Returns null", () => {
+      const key = "SOME_KEY_THAT_IS_NOT_IN_THE_STORAGE";
+      expect(localStorage.getFromLocalStorage(key)).toBeNull();
+    });
+  });
 });
