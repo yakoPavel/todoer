5f23c1edd70bbb6ab20dc45051c930e78676e4bf
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-18 15:28:16 +0500

    Add the `EditLabelForm`

---
+++ src/features/authorizedApp/features/ModalForms/components/EditLabelForm/EditLabelForm.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable dot-notation */
+/* eslint-disable no-param-reassign */
+import { useAppDispatch } from "hooks/storeHooks";
+import React from "react";
+import { actions as uiActions } from "store/slices/ui";
+
+import Form from "../Form/Form";
+
+type FormFieldsConfig = React.ComponentPropsWithoutRef<
+  typeof Form
+>["formFieldsConfig"];
+
+const formFieldsConfig = [
+  {
+    label: "Label name" as const,
+    type: "text" as const,
+    required: true,
+  },
+  {
+    label: "Label color" as const,
+    type: "color" as const,
+    required: true,
+  },
+  {
+    label: "Add to favorites" as const,
+    type: "switch" as const,
+  },
+] as FormFieldsConfig;
+
+const EditLabelForm = () => {
+  const dispatch = useAppDispatch();
+
+  // We should get this data from the state for this concrete label
+  const dummyCurrentData = {
+    "Label name": "My label",
+    "Label color": "gold",
+    "Add to favorites": true,
+  } as Record<string, string | boolean>;
+
+  formFieldsConfig.forEach((staticConfig) => {
+    if (staticConfig.label in dummyCurrentData) {
+      staticConfig.initialValue = dummyCurrentData[staticConfig.label];
+    }
+  });
+
+  const onSubmit = (formValues: Record<string, string | boolean>) => {
+    console.log("Edit label", formValues);
+    dispatch(uiActions.editLabelFormDismissed());
+  };
+
+  const onDismiss = () => {
+    dispatch(uiActions.editLabelFormDismissed());
+  };
+
+  return (
+    <Form
+      title="Edit label"
+      formFieldsConfig={formFieldsConfig}
+      onSubmit={onSubmit}
+      onDismiss={onDismiss}
+    />
+  );
+};
+
+export default EditLabelForm;
