053bd9bec51c4b813a67d352383cdaf7fc5949ca
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-20 14:49:24 +0500

    Add the `useClient` hook
    This hook returns an Axios client instance with the current user's
    auth token set as a 'Authentication' header.

---
+++ src/hooks/useClient.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import useUserContext from "context/UserContext";
+
+/**
+ * Returns an Axios client instance with the current user's auth token
+ * set as a 'Authentication' header.
+ *
+ * @throws Will throw and error if the user is not authenticated.
+ */
+async function useClient() {
+  const user = useUserContext();
+  if (!user) {
+    throw new Error("The user is not authenticated.");
+  }
+
+  const authToken = await user.getIdToken();
+  const instance = axios.create({
+    headers: {
+      Authentication: authToken,
+    },
+  });
+
+  return instance;
+}
+
+export { useClient };
