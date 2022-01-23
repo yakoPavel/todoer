4e66e8d8b6acb96b15ade9f8358f6da439d3f5f4
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-23 19:41:52 +0500

    feat: Prefetch the data in the loading phase

--- src/features/authorizedApp/routes/Main.tsx
+++ src/features/authorizedApp/routes/Main.tsx
@@ -1,8 +1,41 @@
 import React from "react";
 
+import { getLabels } from "../api/getLabels";
+import { getProjects } from "../api/getProjects";
 import { AppHeader } from "../features/AppHeader";
 import { Modals } from "../features/Modals";
 import { SideMenu } from "../features/SideMenu";
+
+import { Loading } from "@/components/Loading/Loading";
+import { useClient } from "@/hooks/useClient";
+import { queryClient } from "@/lib/react-query";
+import { waitForInitialAuthChecking } from "@/utils/authentication";
+
+function useLoadingState() {
+  const [isAuthChecked, setIsAuthChecked] = React.useState(false);
+  const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+
+  const client = useClient();
+
+  React.useEffect(() => {
+    waitForInitialAuthChecking().then(() => setIsAuthChecked(true));
+  }, []);
+
+  React.useEffect(() => {
+    if (!isAuthChecked) return;
+
+    Promise.all([
+      queryClient.fetchQuery("projects", () => getProjects(client)),
+      queryClient.fetchQuery("labels", () => getLabels(client)),
+    ])
+      .then(() => setIsDataLoaded(true))
+      .catch(() => {
+        /* TODO: Remove this catch and let the error boundary to catch the error */
+      });
+  }, [isAuthChecked, client]);
+
+  return !isAuthChecked || !isDataLoaded;
+}
 
 export const Main = () => {
   const [menuState, setMenuState] = React.useReducer(
@@ -10,6 +43,12 @@ export const Main = () => {
       currentState === "opened" ? "closed" : "opened",
     "closed",
   );
+
+  const isLoading = useLoadingState();
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,20 +5,8 @@ import { useUserContext } from "../context/UserContext";
 import { UnauthorizedAppRoutes as AuthorizedAppRoutes } from "./AuthorizedAppRoutes";
 import { UnauthorizedAppRoutes } from "./UnauthorizedAppRoutes";
 
-import { Loading } from "@/components/Loading/Loading";
-import { waitForInitialAuthChecking } from "@/utils/authentication";
-
 export const AppRoutes = () => {
   const user = useUserContext();
-  const [initialAuthChecking, setInitialAuthChecking] = React.useState(true);
-
-  React.useEffect(() => {
-    waitForInitialAuthChecking().then(() => setInitialAuthChecking(false));
-  }, []);
-
-  if (initialAuthChecking) {
-    return <Loading />;
-  }
 
   if (!user) {
     return <UnauthorizedAppRoutes />;
