b53ec3386ef81820ed79104d2e73c4dd6ddb6458
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-24 15:56:10 +0500

    feat: Connect the `AddLabelForm` to the real API

--- src/features/authorizedApp/features/Modals/components/AddLabelForm/AddLabelForm.tsx
+++ src/features/authorizedApp/features/Modals/components/AddLabelForm/AddLabelForm.tsx
@@ -1,32 +1,38 @@
 import React from "react";
 
+import { FormValues } from "../../types";
 import { Form } from "../Form/Form";
 
+import { useCreateLabel } from "@/features/authorizedApp/api/createLabel";
 import { useAppDispatch } from "@/hooks/storeHooks";
 import { actions as uiActions } from "@/store/slices/ui";
 
 const formFieldsConfig = [
   {
     label: "Label name" as const,
+    name: "name" as const,
     type: "text" as const,
     required: true,
   },
   {
     label: "Label color" as const,
+    name: "color" as const,
     type: "color" as const,
     required: true,
   },
   {
     label: "Add to favorites" as const,
+    name: "isFavorite" as const,
     type: "switch" as const,
   },
 ];
 
 export const AddLabelForm = () => {
   const dispatch = useAppDispatch();
+  const createLabelMutation = useCreateLabel();
 
-  const onSubmit = (formValues: Record<string, string | boolean>) => {
-    console.log("New label", formValues);
+  const onSubmit = (formValues: FormValues<typeof formFieldsConfig>) => {
+    createLabelMutation.mutate(formValues);
     dispatch(uiActions.addLabelFormDismissed());
   };
 
