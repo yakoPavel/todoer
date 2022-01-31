410740519599df7ee755feeaf7fe0c2dd0ed6905
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-31 11:17:59 +0500

    test: Bootstrap the test server before tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "@/test/server/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
