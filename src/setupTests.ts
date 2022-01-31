941c3cc9052c1a13a8033acaac51c5bde21d44f7
Pavel Yakovlev <xcvb2@yandex.ru>
2022-01-31 21:43:37 +0500

    test: Add a global cleanup after each test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,8 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
+import { queryClient } from "@/lib/react-query";
+import { resetDb } from "@/test/server/db";
 import { server } from "@/test/server/server";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
+
+afterEach(async () => {
+  queryClient.clear();
+  resetDb();
+});
