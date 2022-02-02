10c005065d5f60173851f17f595a25c337bbca4d
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-02 12:11:29 +0500

    test: Add tests for the `ThemeSwitcher`

---
+++ src/features/authorizedApp/features/ThemeSwitcher/components/tests/ThemeSwitcher.test.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { ThemeSwitcher } from "../ThemeSwitcher/ThemeSwitcher";
+
+import * as themes from "@/style/colors";
+import { render, screen, userEvent, within } from "@/test/testUtils";
+
+const themeNames = Object.keys(themes).map((themeName) =>
+  themeName.replace("Theme", ""),
+);
+
+describe("The `ThemeSwitcher` component", () => {
+  test("Renders buttons for all the themes", () => {
+    render(<ThemeSwitcher />);
+
+    const themeButtons = screen.getAllByRole("radio");
+    expect(themeButtons).toHaveLength(themeNames.length);
+
+    themeNames.forEach((themeName) => {
+      expect(
+        screen.getByRole("radio", { name: themeName }),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe("When a button is clicked", () => {
+    test("Marks the theme as checked", () => {
+      render(<ThemeSwitcher />);
+
+      const secondButton = screen.getAllByRole("radio")[1];
+      userEvent.click(secondButton);
+
+      expect(secondButton).toBeChecked();
+    });
+
+    test("Shows a checkmark sign inside this button", () => {
+      render(<ThemeSwitcher />);
+
+      const secondButton = screen.getAllByRole("radio")[1];
+      userEvent.click(secondButton);
+
+      expect(within(secondButton).getByTestId("checkmark")).toBeInTheDocument();
+    });
+  });
+});
