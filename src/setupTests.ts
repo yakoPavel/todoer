4bd41a3bce5b7fef02deabf57b705d906101705d
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-09 18:43:02 +0500

    test: Add polyfills

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -28,17 +28,37 @@ beforeAll(() => {
 });
 
 beforeAll(() => {
-  Object.defineProperty(global, "matchMedia", {
-    writable: true,
-    value: jest.fn().mockImplementation((query) => ({
-      matches: false,
-      media: query,
-      onchange: null,
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
-      addEventListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
+  Object.defineProperties(global, {
+    matchMedia: {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    },
+    ResizeObserver: {
+      value: class ResizeObserver {
+        observe = jest.fn();
+        unobserve = jest.fn();
+      },
+    },
+    DOMRect: {
+      value: {
+        fromRect: () => ({
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          width: 0,
+          height: 0,
+        }),
+      },
+    },
   });
 });
