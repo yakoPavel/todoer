8288bd33880f3c0c3abaa42ff88b2075ecbea966
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-12 14:15:51 +0500

    fix: Get rid of `[tasks, { project: ...} ]` keys

--- src/features/authorizedApp/api/task/getTasks.ts
+++ src/features/authorizedApp/api/task/getTasks.ts
@@ -3,16 +3,3 @@ import { generateGetItemsQuery } from "../utils/getItems/generateGetItemsQuery";
 
 export const { getItems: getTasks, useItems: useTasks } =
   generateGetItemsQuery<Task>({ dataLabel: ["tasks"], endpoint: "/tasks" });
-
-export const useTasksForProject = (projectId: string) => {
-  const params = new URLSearchParams();
-  params.set("projectId", projectId);
-
-  const { useItems } = generateGetItemsQuery<Task>({
-    dataLabel: ["tasks", { project: projectId }],
-    dataLabelForItem: ["tasks"],
-    endpoint: "/tasks",
-  });
-
-  return useItems(params);
-};
--- src/utils/prefetchData.ts
+++ src/utils/prefetchData.ts
@@ -1,5 +1,4 @@
 import { AxiosInstance } from "axios";
-import { groupBy } from "lodash";
 
 import { getProjects, getLabels, getTasks } from "@/features/authorizedApp/api";
 import { queryClient } from "@/lib/react-query";
@@ -13,18 +12,6 @@ function setIndividualItemsData<T extends { id: string }>(
   });
 }
 
-function setTasksForProjects<T extends { projectId: string }>(tasksData: T[]) {
-  const tasksGroupedByProjects = groupBy(tasksData, (task) => task.projectId);
-  Object.entries(tasksGroupedByProjects).forEach(
-    ([projectId, tasksForTheProject]) => {
-      queryClient.setQueryData(
-        ["tasks", { project: projectId }],
-        tasksForTheProject,
-      );
-    },
-  );
-}
-
 export async function prefetchData(client: Promise<AxiosInstance>) {
   const [projectsData, labelsData, tasksData] = await Promise.all([
     queryClient.fetchQuery("projects", () => getProjects(client)),
@@ -35,6 +22,4 @@ export async function prefetchData(client: Promise<AxiosInstance>) {
   setIndividualItemsData(projectsData, "projects");
   setIndividualItemsData(labelsData, "labels");
   setIndividualItemsData(tasksData, "tasks");
-
-  setTasksForProjects(tasksData);
 }
