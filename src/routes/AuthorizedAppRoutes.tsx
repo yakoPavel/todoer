5dfb460908061a7a0e35b72626c3160588c05c49
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-16 20:05:52 +0500

    feat: Add a route to the `Project` screen

--- src/routes/AuthorizedAppRoutes.tsx
+++ src/routes/AuthorizedAppRoutes.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { Main } from "@/features/authorizedApp";
+import { Project } from "@/features/authorizedApp/features/Project";
 
 export const UnauthorizedAppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Main />}>
+        <Route path="projects/:projectId" element={<Project />} />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
