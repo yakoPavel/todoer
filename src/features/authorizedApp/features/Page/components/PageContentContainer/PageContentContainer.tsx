5151f639b433899888802ccf0aecd4eb24510e2c
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-18 19:44:59 +0500

    feat: Add an overlay to side menu on small screens

--- src/features/authorizedApp/features/Page/components/PageContentContainer/PageContentContainer.tsx
+++ src/features/authorizedApp/features/Page/components/PageContentContainer/PageContentContainer.tsx
@@ -3,25 +3,42 @@ import React from "react";
 
 import { selectors as sideMenuUiSelectors } from "@/features/authorizedApp/store/slices/sideMenuUi";
 import { useAppSelector } from "@/hooks/storeHooks";
+import { useMedia } from "@/hooks/useMedia";
 
 const StyledContainer = styled.div`
   color: ${({ theme }) => theme.text};
 
   height: calc(100vh - var(--header-height, 0) - 1px);
   transition: margin-left 0.3s;
+  position: relative;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 3;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 
 export const PageContentContainer: React.FC = ({ children }) => {
   const isSideMenuOpened = useAppSelector(sideMenuUiSelectors.selectIsOpened);
   const sideMenuWidth = useAppSelector(sideMenuUiSelectors.selectWidth);
 
+  const isSmallScreen = useMedia("(max-width: 48em)");
+
   return (
     <StyledContainer
       style={{
-        marginLeft: `${isSideMenuOpened ? sideMenuWidth : 0}px`,
+        marginLeft: `${
+          isSideMenuOpened && !isSmallScreen ? sideMenuWidth : 0
+        }px`,
       }}
     >
       {children}
+      {isSideMenuOpened && isSmallScreen && <Overlay />}
     </StyledContainer>
   );
 };
