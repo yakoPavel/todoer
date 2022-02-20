e1503b8b6e8f6a14bb3f9eb2c3a5d00942c1f50a
Pavel Yakovlev <xcvb2@yandex.ru>
2022-02-20 19:03:38 +0500

    test: Refactor the `AppHeader` tests

--- src/features/authorizedApp/features/AppHeader/components/AppHeader/tests/AppHeader.test.tsx
+++ src/features/authorizedApp/features/AppHeader/components/AppHeader/tests/AppHeader.test.tsx
@@ -6,26 +6,66 @@ import { render, screen } from "@/test/testUtils";
 
 jest.mock("@/context/UserContext");
 
-test("renders all the header content", () => {
+function renderComponent() {
   render(<AppHeader isSideMenuOpened={false} />);
 
-  expect(
-    screen.getByRole("button", { name: /open the menu/i }),
-  ).toBeInTheDocument();
+  const getToggleMenuButton = () =>
+    screen.getByRole("button", { name: /open the menu/i });
+  const getGoHomeButton = () =>
+    screen.getByRole("button", { name: /open the menu/i });
+  const getQuickAddButton = () =>
+    screen.getByRole("button", { name: /quick add/i });
+  const getChangeThemeButton = () =>
+    screen.getByRole("button", { name: /change the theme/i });
+  const getLogoutButton = () => screen.getByRole("button", { name: /logout/i });
+  const getSearch = () => screen.getByRole("search");
 
-  expect(
-    screen.getByRole("button", { name: /go to home/i }),
-  ).toBeInTheDocument();
+  return {
+    getToggleMenuButton,
+    getGoHomeButton,
+    getQuickAddButton,
+    getChangeThemeButton,
+    getLogoutButton,
+    getSearch,
+  };
+}
 
-  expect(
-    screen.getByRole("button", { name: /quick add/i }),
-  ).toBeInTheDocument();
+describe("The `AppHeader` component", () => {
+  describe("Rendering", () => {
+    test("Renders the 'Toggle menu' button", () => {
+      const { getToggleMenuButton } = renderComponent();
 
-  expect(
-    screen.getByRole("button", { name: /change the theme/i }),
-  ).toBeInTheDocument();
+      expect(getToggleMenuButton()).toBeInTheDocument();
+    });
 
-  expect(screen.getByRole("button", { name: /logout/i })).toBeInTheDocument();
+    test("Renders the 'Go home' button", () => {
+      const { getGoHomeButton } = renderComponent();
 
-  expect(screen.getByRole("search")).toBeInTheDocument();
+      expect(getGoHomeButton()).toBeInTheDocument();
+    });
+
+    test("Renders the 'Quick add' button", () => {
+      const { getQuickAddButton } = renderComponent();
+
+      expect(getQuickAddButton()).toBeInTheDocument();
+    });
+
+    test("Renders the 'Change theme' button", () => {
+      const { getChangeThemeButton } = renderComponent();
+
+      expect(getChangeThemeButton()).toBeInTheDocument();
+    });
+
+    test("Renders the 'Logout' button", () => {
+      const { getLogoutButton } = renderComponent();
+
+      expect(getLogoutButton()).toBeInTheDocument();
+    });
+
+    test("Renders the search", () => {
+      const { getSearch } = renderComponent();
+
+      expect(getSearch()).toBeInTheDocument();
+    });
+  });
 });
